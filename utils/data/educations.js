7adf2223a179b403e9a1a37af5359d9e921e1ef8
yeab14 <yeab149@gmail.com>
2024-06-28 16:28:58 +0300

    education modification done

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2020 - 2023",
+    institution: "Unity University",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2018 - 2019",
+    institution: "Lideta Catholica Cathedral School",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    institution: "kebena Adventist School",
   }
 ]
