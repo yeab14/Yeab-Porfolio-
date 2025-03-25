c0bc974e05df031f7224846bf250cfcafbd7aa08
yeab14 <155532389+yeab14@users.noreply.github.com>
2025-03-25 14:09:35 +0200

    some modifications

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,27 +1,35 @@
 // @flow strict
 import Link from 'next/link';
-import { CgGitFork } from "react-icons/cg";
-import { IoStar } from "react-icons/io5";
 
 function Footer() {
   return (
-    <div className="relative border-t bg-[#0d1224] border-[#353951] text-white text-center py-8">
-      <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
-        <div className="flex justify-center">
-          <div className="absolute top-0 h-[1px] w-1/2 bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
-        </div>
-        <div className="flex flex-col md:flex-row items-center justify-center">
-          <p className="text-lg md:text-xl">
-            © Developer Portfolio by{' '}
-            <Link href="https://www.linkedin.com/in/abu-said-bd/" target="_blank" className="text-[#16f2b3] hover:underline">
-              Yeabsira Dereje
-            </Link>
-          </p>
-        </div>
+    <div className="bg-[#0d1224] text-white py-12">
+      <div className="container mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
 
+        {/* Footer Content */}
+        <div className="flex flex-col items-center text-center space-y-4">
+          
+          {/* Copyright */}
+          <p className="text-1xl font-semibold">
+            © {new Date().getFullYear()}{' '}
+            <span className="text-[#16f2b3] hover:underline">
+              Developer Portfolio by{' '}
+              <Link href="https://www.linkedin.com/in/yeabsira-dereje-b798a9287/" target="_blank">
+                Yeabsira Dereje
+              </Link>
+            </span>
+          </p>
+
+          {/* Tagline */}
+          <p className="text-md text-[#a1a7b3]">
+            Passionate about building scalable, high-performance applications that drive business growth and innovation.
+          </p>
+
+        </div>
       </div>
     </div>
   );
 }
 
 export default Footer;
+
--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -1,5 +1,5 @@
 // @flow strict
-'use client'
+'use client';
 
 import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
@@ -12,196 +12,181 @@ import { SiLeetcode } from "react-icons/si";
 
 function HeroSection() {
   return (
-    <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
+    <section className="relative flex flex-col items-center justify-between py-4 lg:py-12 bg-gradient-to-r from-[#121212] to-[#090909]">
+
+      {/* Background Image with subtle animation */}
       <Image
         src="/hero.svg"
         alt="Hero"
         width={1572}
         height={795}
-        className="absolute -top-[98px] -z-10"
+        className="absolute top-0 -z-10 opacity-40 animate-pulse blur-xl"
       />
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-      <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-  <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-5xl lg:leading-14 animate-float">
-    Hello, <br />
-    I&apos;m {' '}
-    <span className="text-pink-500">{personalData.name}</span>, <br />
-    {`a Professional `}
-    <span className="text-green-400">{personalData.designation}</span>
-    .
-  </h1>
+      <div className="grid grid-cols-1 items-center lg:grid-cols-2 lg:gap-12 gap-y-8 max-w-7xl px-4 sm:px-8 lg:px-16">
+        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-4 md:pb-10 lg:pt-10">
+          <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-600 md:text-6xl lg:text-7xl animate-float">
+            Hello, <br />
+            I&apos;m <span className="text-white">{personalData.name}</span>, <br />
 
-  <div className="my-12 flex items-center gap-5">
-    <Link
-      href={personalData.github}
-      target='_blank'
-      className="transition-all text-pink-500 hover:text-white duration-300 transform hover:scale-110"
-    >
-      <BsGithub size={30} />
-    </Link>
-    <Link
-      href={personalData.linkedIn}
-      target='_blank'
-      className="transition-all text-pink-500 hover:text-white duration-300 transform hover:scale-110"
-    >
-      <BsLinkedin size={30} />
-    </Link>
-    <Link
-      href={personalData.facebook}
-      target='_blank'
-      className="transition-all text-pink-500 hover:text-white duration-300 transform hover:scale-110"
-    >
-      <FaFacebook size={30} />
-    </Link>
-    <Link
-      href={personalData.leetcode}
-      target='_blank'
-      className="transition-all text-pink-500 hover:text-white duration-300 transform hover:scale-110"
-    >
-      <SiLeetcode size={30} />
-    </Link>
-    <Link
-      href={personalData.twitter}
-      target='_blank'
-      className="transition-all text-pink-500 hover:text-white duration-300 transform hover:scale-110"
-    >
-      <FaTwitterSquare size={30} />
-    </Link>
+            <span className="text-green-400">{personalData.designation}</span>.
+          </h1>
+
+          <div className="my-12 flex items-center gap-6">
+            {['github', 'linkedIn', 'facebook', 'leetcode', 'twitter'].map((platform) => (
+              <Link
+                href={personalData[platform]}
+                target="_blank"
+                key={platform}
+                className="text-pink-500 hover:text-white transition-all duration-300 transform hover:scale-110"
+              >
+                {
+                  platform === 'github' ? <BsGithub size={30} /> :
+                  platform === 'linkedIn' ? <BsLinkedin size={30} /> :
+                  platform === 'facebook' ? <FaFacebook size={30} /> :
+                  platform === 'leetcode' ? <SiLeetcode size={30} /> :
+                  <FaTwitterSquare size={30} />
+                }
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Link href="#contact" className="bg-gradient-to-r from-pink-500 to-violet-600 p-[1px] rounded-full flex-1 transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
+              <button className="w-full px-6 py-3 bg-[#0d1224] rounded-full text-white uppercase tracking-wider font-medium text-sm flex items-center justify-center hover:bg-transparent hover:border-pink-500 hover:text-white transition-all duration-200 ease-out transform hover:shadow-xl">
+                <span>Contact me</span>
+                <RiContactsFill size={14} className="ml-2" />
+              </button>
+            </Link>
+
+            <Link href={personalData.resume} target="_blank" className="flex items-center gap-2 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-6 py-3 text-white uppercase tracking-wider font-medium text-sm hover:text-white hover:from-violet-600 hover:to-pink-500 transition-all duration-200 ease-out transform hover:shadow-xl">
+              <span>Get Resume</span>
+              <MdDownload size={14} className="ml-2" />
+            </Link>
+          </div>
+        </div>
+
+        {/* Terminal-like code block with animated typing */}
+      {/* Terminal-like code block with animated typing */}
+<div className="order-1 lg:order-2 relative rounded-lg bg-gradient-to-r from-[#121212] to-[#090909] border-[#1b2c68a0] shadow-lg hover:shadow-2xl transition-shadow duration-500 ease-in-out transform hover:-translate-y-2">
+  <div className="absolute inset-0 flex justify-between">
+    <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600 animate-move-left-right"></div>
+    <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent animate-move-left-right"></div>
   </div>
 
-  <div className="flex items-center gap-3">
-    <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full flex-1 transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-      <button className="w-full px-3 py-2 md:px-6 md:py-3 bg-[#0d1224] rounded-full text-white uppercase tracking-wider font-medium text-sm flex items-center justify-center hover:bg-transparent hover:border-pink-500 hover:text-white transition-all duration-200 ease-out transform hover:shadow-purple-500">
-        <span>Contact me</span>
-        <RiContactsFill size={14} className="ml-1" />
-      </button>
-    </Link>
+  <div className="px-6 lg:px-8 py-6 lg:py-8">
+    <div className="flex items-center space-x-2">
+      <div className="h-3 w-3 rounded-full bg-red-400 animate-move-left-right"></div>
+      <div className="h-3 w-3 rounded-full bg-orange-400 animate-move-left-right"></div>
+      <div className="h-3 w-3 rounded-full bg-green-200 animate-move-left-right"></div>
+    </div>
+  </div>
 
-    <Link href={personalData.resume} target="_blank" className="flex items-center gap-2 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 py-2 md:px-6 md:py-3 text-white uppercase tracking-wider font-medium text-sm hover:text-white hover:from-violet-600 hover:to-pink-500 transition-all duration-200 ease-out transform hover:shadow-purple-500">
-      <span>Get Resume</span>
-      <MdDownload size={14} className="ml-1" />
-    </Link>
+  <div className="overflow-hidden border-t-[2px] border-indigo-900 px-6 lg:px-8 py-6 lg:py-8">
+    <code className="font-mono text-xs md:text-sm lg:text-base text-white">
+      <div>
+        <span className="text-pink-500">const </span>
+        <span className="text-white">softwareEngineer</span>
+        <span className="text-pink-500"> = </span>
+        <span className="text-gray-400">{'{'}</span>
+      </div>
+      <div>
+        <span className="ml-6 text-white">name:</span>
+        <span className="text-gray-400">{`'`}</span>
+        <span className="text-amber-300">Yeabsira Dereje</span>
+        <span className="text-gray-400">{`',`}</span>
+      </div>
+      <div className="ml-6">
+        <span className="text-white">skills:</span>
+        <span className="text-gray-400">{`[ `}</span>
+        <span className="text-amber-300">React</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">NextJS</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Vue.js</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Spring Boot</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Node.js</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">FastAPI</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Python</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Golang</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Docker</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Kubernetes</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">MySQL</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">PostgreSQL</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">MongoDB</span>
+        <span className="text-gray-400">{` ],`}</span>
+      </div>
+      <div>
+        <span className="ml-6 text-white">experience:</span>
+        <span className="text-gray-400">{`[ `}</span>
+        <span className="text-amber-300">Building scalable e-commerce platforms</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Developing robust backend architectures</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Leading team-driven projects</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-amber-300">Optimizing performance at scale</span>
+        <span className="text-gray-400">{` ],`}</span>
+      </div>
+      <div>
+        <span className="ml-6 text-white">qualities:</span>
+        <span className="text-gray-400">{`[ `}</span>
+        <span className="text-orange-400">Quick Learner</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-orange-400">Highly Committed</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-orange-400">Team-Oriented</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-orange-400">Creative Problem Solver</span>
+        <span className="text-gray-400">{`, `}</span>
+        <span className="text-orange-400">Versatile Skillset</span>
+        <span className="text-gray-400">{` ],`}</span>
+      </div>
+      <div>
+        <span className="ml-6 text-white">readyForOpportunities:</span>
+        <span className="text-orange-400">function</span>
+        <span className="text-gray-400">{'() {'}</span>
+      </div>
+      <div>
+        <span className="ml-8 text-cyan-400">return</span>
+        <span className="text-gray-400">{`(`}</span>
+      </div>
+      <div>
+        <span className="ml-12 text-cyan-400">this.</span>
+        <span className="text-white">dedicatedWorker</span>
+        <span className="text-amber-300">&amp;&amp;</span>
+      </div>
+      <div>
+        <span className="ml-12 text-cyan-400">this.</span>
+        <span className="text-white">creativeProblemSolver</span>
+        <span className="text-amber-300">&amp;&amp;</span>
+      </div>
+      <div>
+        <span className="ml-12 text-cyan-400">this.</span>
+        <span className="text-white">versatileSkills</span>
+        <span className="text-amber-300">&gt;=</span>
+        <span className="text-orange-400">5</span>
+      </div>
+      <div><span className="ml-8 text-gray-400">{`)`}</span></div>
+      <div><span className="ml-6 text-gray-400">{`};`}</span></div>
+      <div><span className="text-gray-400">{`};`}</span></div>
+    </code>
   </div>
 </div>
 
-
-
-        <div className="order-1 lg:order-2 relative rounded-lg bg-gradient-to-r from-[#0d1224] to-[#0a0d37] border-[#1b2c68a0] shadow-xl hover:shadow-purple-500 transition-shadow duration-500 ease-in-out transform hover:-translate-y-1">
-      <style jsx>{`
-        @keyframes move-left-right {
-          0%, 100% {
-            transform: translateX(0);
-          }
-          50% {
-            transform: translateX(0px);
-          }
-        }
-
-        .animate-move-left-right {
-          animation: move-left-right 1s infinite;
-        }
-      `}</style>
-      <div className="absolute inset-0 flex justify-between">
-        <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600 animate-move-left-right"></div>
-        <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent animate-move-left-right"></div>
       </div>
-      <div className="px-4 lg:px-8 py-5">
-        <div className="flex items-center space-x-2">
-          <div className="h-3 w-3 rounded-full bg-red-400 animate-move-left-right"></div>
-          <div className="h-3 w-3 rounded-full bg-orange-400 animate-move-left-right"></div>
-          <div className="h-3 w-3 rounded-full bg-green-200 animate-move-left-right"></div>
-        </div>
-      </div>
-      <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-        <code className="font-mono text-xs md:text-sm lg:text-base">
-          <div className="blink">
-            <span className="text-pink-500">const&nbsp; </span>
-            <span className="text-white">coder</span>
-            <span className="text-pink-500">=</span>
-            <span className="text-gray-400">{'{'}</span>
-          </div>
-          <div>
-            <span className="ml-4 lg:ml-8 text-white">name:</span>
-            <span className="text-gray-400">{`'`}</span>
-            <span className="text-amber-300">Yeabsira Dereje</span>
-            <span className="text-gray-400">{`',`}</span>
-          </div>
-          <div className="ml-4 lg:ml-8">
-            <span className="text-white">skills:</span>
-            <span className="text-gray-400">{`['`}</span>
-            <span className="text-amber-300">React</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">NextJS</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Redux</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Apollo Client</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">NestJS</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">REST API</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">GraphQL</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Python</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">Streamlit</span>
-            <span className="text-gray-400">{"', '"}</span>
-            <span className="text-amber-300">FastAPI</span>
-            <span className="text-gray-400">{"'],"}</span>
-          </div>
-          <div>
-            <span className="ml-4 lg:ml-8 text-white">quickLearner:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          <div>
-            <span className="ml-4 lg:ml-8 text-white">highlyCommitted:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          <div>
-            <span className="ml-4 lg:ml-8 text-white">teamOriented:</span>
-            <span className="text-orange-400">true</span>
-            <span className="text-gray-400">,</span>
-          </div>
-          <div>
-            <span className="ml-4 lg:ml-8 text-green-400">readyForOpportunities:</span>
-            <span className="text-orange-400">function</span>
-            <span className="text-gray-400">{'() {'}</span>
-          </div>
-          <div>
-            <span className="ml-8 lg:ml-16 text-orange-400">return</span>
-            <span className="text-gray-400">{`(`}</span>
-          </div>
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="text-white">dedicatedWorker</span>
-            <span className="text-amber-300">&amp;&amp;</span>
-          </div>
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="text-white">creativeProblemSolver</span>
-            <span className="text-amber-300">&amp;&amp;</span>
-          </div>
-          <div>
-            <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-            <span className="text-white">versatileSkills</span>
-            <span className="text-amber-300">&gt;=</span>
-            <span className="text-orange-400">5</span>
-          </div>
-          <div><span className="ml-8 lg:ml-16 text-gray-400">{`)`}</span></div>
-          <div><span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span></div>
-          <div><span className="text-gray-400">{`};`}</span></div>
-        </code>
-      </div>
-    </div>
-    </div>
     </section>
-    
   );
 };
 
 export default HeroSection;
-
